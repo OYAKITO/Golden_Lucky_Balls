c78ee581595f24fa88bd39e2dbbe4193733877a4
Seijuro-V <147134893+Seijuro-V@users.noreply.github.com>
2024-04-03 17:03:05 +0800

    for database Pseudo-random numbers

--- admin/user/urls.py
+++ admin/user/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from . import views
+from . import views 
+from . views import  Generated_lotto_draw 
+#we apply the url pattern for lotto draw endpoint
 
 urlpatterns = [path('', views.home, name= 'Home'),
-               path('signup/', views.sign_up, name= 'SignUp')]
+               path('signup/', views.sign_up, name= 'SignUp'),
+               path('lotto/', views.Generated_lotto_draw, name='Generated_lotto_draw')]#This line of code consist of /draw/(url pattern end with draw).
+                                                                               #'generate_lotto_draw'function request is refers to 'generate_lotto'from views.py.
+                                                                               # 'name='generate_lotto_draw' can use to reference in Django templates.
