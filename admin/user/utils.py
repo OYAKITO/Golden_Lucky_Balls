83c7425dcc9f805fd54bfd3f345c2724f5e06af7
Seijuro-V <147134893+Seijuro-V@users.noreply.github.com>
2024-04-03 16:31:44 +0800

    For Database Pseudo-radom numbers

---
+++ admin/user/utils.py
@@ -0,0 +1,9 @@
+import random
+
+#create Pseudo-random numbers generate function
+
+def Generated_lotto_draw(): #funtion name
+    number =  sorted(random.sample(range(1, 51), 6))
+    #list that generate 6 unique numbers between 1 to 51
+    return ', '.join(str(num) for num in numbers) 
+    #return a string with containing the lotto numbers with ',' seperator
