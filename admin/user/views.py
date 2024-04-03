cea4836248346552c7c7abfd92cba65eb74393d6
Seijuro-V <147134893+Seijuro-V@users.noreply.github.com>
2024-04-03 16:56:09 +0800

    for database Pseudo-random numbers

--- admin/user/views.py
+++ admin/user/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse #for return JSON data in HTTP response
-from .utils import generate_lotto_draw #to function import of utils.py
+from .utils import Generated_lotto_draw #to function import of utils.py
 from .models import LottoDraw #to function import models
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
 def sign_up(request):
     return render(request, 'signup.html')
-def generate_lotto_draw(request): 
-    #intended to call HTTP request to made specific URL handled by django
+   
+def Generated_lotto_draw(request):  #intended to call HTTP request to made specific URL handled by django
     numbers = generate_lotto_draw 
     #we use ORM(Object-relational Mapper)"parang object-oriented interface para sa python" para sa lotto draw objec with generated numbers
     lottoDraw.objects.create(numbers = numbers) 
